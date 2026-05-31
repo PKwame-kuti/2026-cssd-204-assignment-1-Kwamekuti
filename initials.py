f5a096919abd0e8d24cb85ac20259866e23c53aa
Prince Kwame Kuti <2425403338@live.gctu.edu.gh>
2026-05-31 04:31:24 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,33 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Prince Kwame Kuti
+# STUDENT ID: 2425403338
+# Email: 2425403338@live.gctu.edu.gh
+# Grader: AUGUSTUS
+#
+# On my honor, Prince Kwame Kuti, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Print initials PKK as small text and as large block letters (12x10) made of the initial letters and periods
 
+    print()
+    print("...PKK")
+    print()
+    print("...PPPPPPPPPPPP........KK........KK........KK........KK.....")
+    print("...PP........PP........KK......KK..........KK.......KK......")
+    print("...PP........PP........KK....KK............KK....KK.........")
+    print("...PP........PP........KK..KK..............KK..KK...........")
+    print("...PP........PP........KK.KK...............KK.KK............")
+    print("...PPPPPPPPPPPP........KK.KK...............KK.KK............")
+    print("...PP..................KK..KK..............KK..KK...........")
+    print("...PP..................KK....KK............KK....KK.........")
+    print("...PP.............**...KK......KK.....**...KK......KK.....**")
+    print("...PP.............**...KK........KK...**...KK........KK...**")
+    print()
 
 
 main()
